07d31fb1e914eb9e515081e4fa9e3d0eb0a609f5
Anita <a.nita@hotmail.co.uk>
2015-05-31 13:32:33 +0100

    no changes

--- main.js
+++ main.js
@@ -4,13 +4,19 @@ request.open('GET', 'name.txt', true); //.open(method, url, async) this is synch
 
 request.send(null);
 
-console.log(request.responseText);
+// console.log(request.responseText);
 
 // document.getElementById('response').innerHTML = request.responseText; this is was used to synchrounsly call responseText
 
 request.onreadystatechange = function() {
 	if (request.readyState === 4) {
-		console.log(request);
+		// console.log(request);
 		document.getElementById('response').innerHTML = request.responseText;
 	};
 };
+
+
+
+
+
+
